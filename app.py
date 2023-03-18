4290b3444c43fa1ef68e04c05504852f9b9a6dd1
[Varsha Mane] <[manevd0903@gmail.com]>
2023-03-18 21:41:25 +0530

    Added different images

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello_world():
 def products():
     return 'We have this products!!'
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
