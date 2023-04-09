0ec913b5ebe8c6d1fca0787bbfc6314f826b01a7
[Varsha Mane] <[manevd0903@gmail.com]>
2023-04-09 18:41:24 +0530

    added all details for cources

--- app.py
+++ app.py
@@ -9,10 +9,24 @@ def hello_world():
 def products():
     return 'We have this products!!'
 
-@app.route('/home')
+@app.route('/home/')
 def home():
     return render_template('home.html')
 
+@app.route('/home/pythondetails')
+def pythondetails():
+    return render_template('pythondtl.html')
 
+@app.route('/home/salesforcedetails')
+def salesforcedetails():
+    return render_template('salesforcedtl.html')
+
+@app.route('/home/manualtestingdetails')
+def manualtestingdetails():
+    return render_template('manualtestingdtl.html')
+
+@app.route('/home/automationtestingdetails')
+def automationtestingdetails():
+    return render_template('automationdtl.html')
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
